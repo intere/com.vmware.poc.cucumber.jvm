c38e5fe5a84eec7c71c7a7f781ee5f785d830232
intere <intere@gmail.com>
2012-09-06 18:09:54 -0500

    I have 1 working test scenario.

--- src/test/java/com/vmware/poc/cucumber/jvm/TomcatStartupStepDefs.java
+++ src/test/java/com/vmware/poc/cucumber/jvm/TomcatStartupStepDefs.java
@@ -19,45 +19,49 @@ import cucumber.annotation.en.When;
  */
 public class TomcatStartupStepDefs {
 	
+	private static final String TOMCAT_PROCESS = ".*tomcat.*";
+	
 	private static final Logger LOG = Logger.getLogger(TomcatStartupStepDefs.class);
-
 	
 	private TestConfig config;
 	private RemoteProcessListing listing;
 	private RemoteTomcatController tomcatController;
+	
 
 	@Given("^Tomcat is not running on \"([^\"]*)\"$")
-	public void Tomcat_is_not_running(String host) throws Throwable {
-		
+	public void Tomcat_is_not_running(String host) throws Throwable 
+	{		
 		// get the config
 		config = SpringLoader.loadTestConfigByName(host);
 		listing = new RemoteProcessListing(config);
 		tomcatController = new RemoteTomcatController(config);
 		
 		// Check to see if tomcat is running.  If it is, then kill it
-		if(listing.hasProcessByRegex("tomcat")) {
+		if(listing.hasProcessByRegex(TOMCAT_PROCESS)) {
 			LOG.info("Tomcat is running on host: " + config.getHost() + ", shutting down");
 			tomcatController.run(RunMethod.Stop);
 		}
 	}
 	
 	@When("^I tell Tomcat to startup$")
-	public void I_tell_Tomcat_to_startup() throws Throwable {
-		
+	public void I_tell_Tomcat_to_startup() throws Throwable 
+	{		
 		LOG.info("Starting up tomcat on host: " + config.getHost());
 		tomcatController.run(RunMethod.Start);
 	}
 	
 	@And("^wait for 10 seconds$")
-	public void wait_for_10_seconds() throws Throwable {
+	public void wait_for_10_seconds() throws Throwable 
+	{
 		Thread.sleep(10000L);
 	}
 	
 	@Then("^Tomcat should be running$")
-	public void Tomcat_should_be_running() throws Throwable {
-		
+	public void Tomcat_should_be_running() throws Throwable 
+	{		
 		listing = new RemoteProcessListing(config);
 		
-		assertTrue("Tomcat is not running on host: " + config.getHost(), listing.hasProcessByRegex("tomcat"));
+		assertTrue("Tomcat is not running on host: " + config.getHost(),
+				listing.hasProcessByRegex(TOMCAT_PROCESS));
 	}
 }
