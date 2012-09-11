32b9b18ca96be1f544c43def6307c75d3529be1c
Scott Frederick <scottyfred@gmail.com>
2012-09-10 19:38:10 -0500

    Added location of features to RunCukesTest.

--- src/test/java/com/vmware/poc/cucumber/jvm/RunCukesTest.java
+++ src/test/java/com/vmware/poc/cucumber/jvm/RunCukesTest.java
@@ -5,7 +5,8 @@ import org.junit.runner.RunWith;
 import cucumber.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@Cucumber.Options(glue = {"com.vmware.poc.cucumber.jvm.steps"},
+@Cucumber.Options(features = "classpath:features",
+		glue = {"com.vmware.poc.cucumber.jvm.steps"},
 		format = {"pretty", "html:target/cucumber-html-report", "json-pretty:target/cucumber-report.json"})
 public class RunCukesTest {
 
