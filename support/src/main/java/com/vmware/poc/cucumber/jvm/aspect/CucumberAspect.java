6419cdcca06fa945a1b21249e3c0ab7372b5d15e
Eric Internicola <einternicola@proteus.local>
2013-01-08 15:12:28 -0700

    Implemented the first basic working version of the AOP (via Aspect-J).

--- support/src/main/java/com/vmware/poc/cucumber/jvm/aspect/CucumberAspect.java
+++ support/src/main/java/com/vmware/poc/cucumber/jvm/aspect/CucumberAspect.java
@@ -1,31 +1,41 @@
 package com.vmware.poc.cucumber.jvm.aspect;
 
 import org.apache.log4j.Logger;
-import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StopWatch;
 
 @Aspect
-@Component("cucumberAspect")
+//@Component("cucumberAspect")
 public class CucumberAspect {
-	
-	private static final Logger LOG = Logger.getLogger(CucumberAspect.class);
-	
+
+	private static final Logger logger = Logger.getLogger(CucumberAspect.class);
+
 	public CucumberAspect() {
-		LOG.info("Cucumber Aspect was created");
+		logger.info("Cucumber Aspect was created");
 	}
 
-	@Pointcut("execution(* cucumber..*(..))")
-	private Object cucumberCutpoint(ProceedingJoinPoint pjp) throws Throwable {
-		StopWatch sw = new StopWatch(getClass().getSimpleName());
-		try {
-			sw.start(pjp.getSignature().getName());
-			return pjp.proceed();
-		} finally {
-			sw.stop();
-			LOG.info(sw.prettyPrint());
-		}
+	@Pointcut ("within(cucumber.runtime.model.*) && execution(* *(..))")
+	public void logging() {}
+
+	@Before(value = "logging()", argNames = "joinPoint")
+	public void enteringMethod(JoinPoint joinPoint) {
+		Signature signature = joinPoint.getSignature();
+		String className = signature.getDeclaringType().getSimpleName();
+		String methodName = signature.getName();
+		logger.info("enteringMethod " + className + "::" + methodName);
 	}
+
+	@AfterReturning(pointcut = "logging()", returning = "returnValue", argNames = "joinPoint,returnValue")
+	public void leavingMethod(JoinPoint joinPoint, Object returnValue) {
+		Signature signature = joinPoint.getSignature();
+		String className = signature.getDeclaringType().getSimpleName();
+		String methodName = signature.getName();
+		logger.info("leavingMethod " + className + "::" + methodName);
+	}
+
 }
